97699fd4c7db47ac54010ca59d442ddf87e409bd
Aravind R <114272941+drew0310@users.noreply.github.com>
2023-05-07 22:32:46 +0530

    Add files via upload

---
+++ Breakout.java
@@ -0,0 +1,16 @@
+import javax.swing.JFrame;
+import brick.*;
+public class Breakout {
+    public static void main(String[] args) {
+        JFrame obj = new JFrame();
+        //Gameplay class contains the logical functioning of the game
+        GamePlay gameplay = new GamePlay();
+        obj.setBounds(10,10,700,600);
+        obj.setTitle("BrickBreaker");
+        obj.setResizable(false);
+        obj.setVisible(true);
+        obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gameplay);
+    }
+    
+}
